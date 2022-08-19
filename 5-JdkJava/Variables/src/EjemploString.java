1f3b7bab4c0271cee3f63bd2276650dfebd2f7da
Carlos <carlosrosario.camilo@gmail.com>
2022-08-18 21:47:26 -0500

    commit del dia 18/08/2022

---
+++ 5-JdkJava/Variables/src/EjemploString.java
@@ -0,0 +1,10 @@
+public class EjemploString {
+    public static void main(String[] args) {
+        String curso = "Curso en Java";
+        System.out.println("curso =" +curso);
+        String nombre = "Carlos Camilo";
+        String resultado = new String ("Curso en java");
+        System.out.println("resultado =" + resultado);
+}
+    }
+
--- 5-JdkJava/Variables/src/EjemploVariables.java
+++ 5-JdkJava/Variables/src/EjemploVariables.java
@@ -1,7 +1,13 @@
-public class EjemploVariables {
-    public static void main(String[] args) {
-        String nombre ="Carlos";
-        String saludo ="Hola esto es ejemplo de variables";
-        char espacio = " a ";
-        System.out.println("espacio = " +espacio);
-        System.out.println(
+//public class EjemploVariables {
+    //public static void main(String[] args) {
+        //String nombre ="Carlos";
+        //String saludo ="Hola esto es ejemplo de variables";
+        //char espacio = ''
+        //char caracter = " a ";
+        //System.out.println("espacio = " +espacio);
+        //System.out.println("char corresponde
+
+    //va miVar = 5;
+
+
+
